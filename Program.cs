d7b94f32990e050bfd0eb0460e68db76b2080fd4
StandbyMe <stand-by-me@mail.com>
2018-04-01 20:52:46 +0800

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace docx_cleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
